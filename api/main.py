db7f96ea0c96d93c2a05560c5430e26d7a761cd6
rothehuman <rodri.gaston20@gmail.com>
2025-06-10 18:41:43 -0300

    rutas cambiadas

--- api/main.py
+++ api/main.py
@@ -16,17 +16,17 @@ import music.cancion.endpoint
 
 app = FastAPI()
 
-prefix_base = "/api/v1"
+# prefix_base = "/api/v1"
 
-app.include_router(user.usuario.endpoint.router, prefix=f"{prefix_base}/usuario")
-app.include_router(user.rol.endpoint.router, prefix=f"{prefix_base}/rol")
-app.include_router(user.rating.endpoint.router, prefix=f"{prefix_base}/rating")
-app.include_router(user.review.endpoint.router, prefix=f"{prefix_base}/review")
-app.include_router(user.coleccion.endpoint.router, prefix=f"{prefix_base}/coleccion")
-app.include_router(user.coleccion_canciones.endpoint.router, prefix=f"{prefix_base}/coleccion_canciones")
+app.include_router(user.usuario.endpoint.router)
+app.include_router(user.rol.endpoint.router)
+app.include_router(user.rating.endpoint.router)
+app.include_router(user.review.endpoint.router)
+app.include_router(user.coleccion.endpoint.router)
+app.include_router(user.coleccion_canciones.endpoint.router)
 
-app.include_router(music.album.endpoint.router, prefix=f"{prefix_base}/album")
-app.include_router(music.genero.endpoint.router, prefix=f"{prefix_base}/genero")
-app.include_router(music.artista.endpoint.router, prefix=f"{prefix_base}/artista")
-app.include_router(music.artista_genero.endpoint.router, prefix=f"{prefix_base}/artista_genero")
-app.include_router(music.cancion.endpoint.router, prefix=f"{prefix_base}/cancion")
+app.include_router(music.album.endpoint.router)
+app.include_router(music.genero.endpoint.router)
+app.include_router(music.artista.endpoint.router)
+app.include_router(music.artista_genero.endpoint.router)
+app.include_router(music.cancion.endpoint.router)
